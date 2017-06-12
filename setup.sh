cbd3c0290e3332e56516fe93ab5d4fb23ffefb21
Ben Krikler <b.krikler@cern.ch>
2017-06-12 14:03:30 +0100

    Improve the setup.sh script

--- setup.sh
+++ setup.sh
@@ -11,3 +11,18 @@ export PYTHONPATH="${ALPHATWIRL}:${ALPHATWIRL_INTERFACE}:${PYTHONPATH}"
 pip install --user -r ${EXAMPLES_ROOT}/requirements.txt
 
 PS1_PREFIX=RA1-ATwirl
+
+DataDir=data
+LocalDataFile=/hdfs/SUSY/RA1/80X/MC/20170129_Summer16_newJECs/AtLogic_MCSummer16_SM/ZJetsToNuNu_HT100to200_madgraph/treeProducerSusyAlphaT/tree.root
+OutLocalDataFile=$(cut -f7,9-11 -d/ <<<"$LocalDataFile" |sed -e 's%/%--%g')
+( 
+mkdir -p "$DataDir"
+cd "$DataDir"
+if [ ! -r "${OutLocalDataFile}" ];then
+    echo "Fetching a local data file for you"
+    (
+    set -x
+    cp "$LocalDataFile" "$OutLocalDataFile"
+    )
+fi
+)
